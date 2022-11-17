e220e73279f5bd6b101abcefe6b1d5eb946e4ac7
krmanski <choromanski117@gmail.com>
2022-11-17 01:45:19 -0500

    Complete project
    This project is a little less complex than some of my other recent
    projects. This was because I wanted to make the focus of this project
    learning how to make a site using webpack, and to challenge myself
    to build a site from nothing but the DOM and one starting div.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import{renderAbout} from './about';
 
 //global variables
 const content = document.getElementById("content");
+let buttonIDs = ["about", "home", "menu"]
 
 
 function renderLayout(){
@@ -25,11 +26,30 @@ function renderLayout(){
     logo.setAttribute('src', 'https://openclipart.org/download/96427/hat.svg');
     logo.setAttribute('alt', 'logo');
     logo.classList.add('logo');
+
+    const btnHome = document.createElement('button');
+    btnHome.classList.add("nav-btn");
+    btnHome.setAttribute('id', 'home');
+    btnHome.innerText = "Home";
+
+    const btnMenu = document.createElement('button');
+    btnMenu.classList.add("nav-btn");
+    btnMenu.setAttribute('id', 'menu');
+    btnMenu.innerText = "Menu";
+
+    const btnAbout = document.createElement('button');
+    btnAbout.classList.add("nav-btn");
+    btnAbout.setAttribute('id', 'about');
+    btnAbout.innerText = "About";
+    
     
 
     const navbar = document.createElement('div');
     navbar.classList.add('navbar', 'flexbox');
     navbar.appendChild(logo);
+    navbar.appendChild(btnHome);
+    navbar.appendChild(btnMenu);
+    navbar.appendChild(btnAbout);
 
     
     const contentBody = document.createElement('div');
@@ -56,67 +76,63 @@ function renderLayout(){
 
 
 //initial homepage load:
-// renderHome();
-// renderMenu();
 renderLayout();
-renderAbout();
+renderHome();
 
 
 
 
 
-// //this will eventually be index.js
-//     //this will eventually be index.js
-//     let nothing = undefined;
-//     let buttonIDs = ["about", "home", "menu"]
-//     let content = document.getElementById("content");
-//     function insertNode(parentID, element, innerText, ID, classes){
-//         const node = document.createElement(element);
-//         node.innerText = innerText;
-//         if (!ID == null){
-//             node.setAttribute('id', ID)
-//         } else{
-//             null;
-//         };
-//         if (!classes == null){
-//             node.setAttribute('class', classes)
-//         } else{
-//             null;
-//         };
-//         document.getElementById(parentID).appendChild(node);  
+
     
-//     };
+    
+function insertNode(parentID, element, innerText, ID, classes){
+    const node = document.createElement(element);
+    node.innerText = innerText;
+    if (!ID == null){
+        node.setAttribute('id', ID)
+    } else{
+        null;
+    };
+    if (!classes == null){
+        node.setAttribute('class', classes)
+    } else{
+        null;
+    };
+    document.getElementById(parentID).appendChild(node);  
 
-//     const clearContent=()=>{
-//         //replaces content area with a blank space to erase all elements in that DOM node
-//         content.textContent = "";
-//     };
+};
 
-//     const loadContent = (navID) =>{
-//         if (navID === "home"){
-            
-//             clearContent();
-//             home.renderHome();
-//         }
-//         if(navID === "menu"){
-//             clearContent();
-//             menu.renderMenu();
-//         }
-//         if(navID === "about"){
-//             clearContent();
-            
-//             about.renderAbout();
-            
-//         };
-//     };
+const clearContent=()=>{
+    //replaces content area with a blank space to erase all elements in that DOM node
+    content.textContent = "";
+};
 
-//     //query selectors and event listners
-//     buttonIDs.forEach(element => {
-//         let navButton = document.getElementById(element);
-//         navButton.addEventListener('click', function(){
-//             loadContent(navButton.id);
-//         });
-//     });
+const loadContent = (navID) =>{
+    if (navID === "home"){
+        
+        clearContent();
+        renderHome();
+    }
+    if(navID === "menu"){
+        clearContent();
+        renderMenu();
+    }
+    if(navID === "about"){
+        clearContent();
+        
+        renderAbout();
+        
+    };
+};
 
-//
+//query selectors and event listners
+buttonIDs.forEach(element => {
+    let navButton = document.getElementById(element);
+    navButton.addEventListener('click', function(){
+        loadContent(navButton.id);
+    });
+});
+
+
     
